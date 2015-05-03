61311015c97104695eb08d4792309cc6576ee0ca
GuillaumeFlambard <guillaume.flambard01@gmail.com>
2015-05-03 18:13:33 +0200

    initialyse structure

---
+++ src/Portfolio/PortfolioBundle/Controller/PortfolioController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Portfolio\PortfolioBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class PortfolioController extends Controller
+{
+    public function indexAction()
+    {
+        //return $this->render('OuiEatFrenchAdminBundle:Admin:index.html.twig');
+        die('test');
+    }
+}
