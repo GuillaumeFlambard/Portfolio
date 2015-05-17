60ee3d92cdbad7857e8fada5500051e69822bc09
GuillaumeFlambard <guillaume.flambard01@gmail.com>
2015-05-17 17:54:32 +0200

    Translation and CV by fancyBox

--- src/Portfolio/PortfolioBundle/Controller/PortfolioController.php
+++ src/Portfolio/PortfolioBundle/Controller/PortfolioController.php
@@ -3,11 +3,43 @@
 namespace Portfolio\PortfolioBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Portfolio\PortfolioBundle\Entity\Message;
 
 class PortfolioController extends Controller
 {
     public function indexAction()
     {
         return $this->render('PortfolioPortfolioBundle:Portfolio:portfolio.html.twig');
+    }
+
+    public function setLangAction($lang)
+    {
+        $languages = array('fr','en');
+        if (in_array($lang, $languages))
+        {
+            $this->container->get('request')->setLocale($lang);
+        }
+        return $this->indexAction();
+    }
+
+    public function setMessageAction()
+    {
+        $request = $this->getRequest();
+
+        if($request->isXmlHttpRequest())
+        {
+            $email = $request->request->get('email');
+            $comment = $request->request->get('comment');
+            //$message = new Message;
+            //$message->setEmail($email);
+            //$message->setComment($comment);
+
+            //$em = $this->getDoctrine()->getManager();
+            //$em->persist($message);
+            //$em->flush();
+        }
+
+        return new JsonResponse();
     }
 }
---
+++ src/Portfolio/PortfolioBundle/Entity/Message.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace Portfolio\PortfolioBundle\Entity;
+
+use Symfony\Component\Validator\Constraints as Assert;
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+
+/**
+ * Message
+ *
+ * @ORM\Table(name="message")
+ * @ORM\Entity()
+ */
+class Message
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @var string
+     *
+     * @Assert\NotBlank()
+     * @ORM\Column(name="email", type="string", length=100)
+     */
+    protected $email;
+
+    /**
+     * @var string
+     *
+     * @Assert\NotBlank()
+     * @ORM\Column(name="comment", type="string")
+     */
+    protected $comment;
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     * @return Message
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set comment
+     *
+     * @param string $comment
+     * @return Message
+     */
+    public function setComment($comment)
+    {
+        $this->comment = $comment;
+
+        return $this;
+    }
+
+    /**
+     * Get comment
+     *
+     * @return string
+     */
+    public function getComment()
+    {
+        return $this->comment;
+    }
+}
